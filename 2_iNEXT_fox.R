ee51f0a24d2eff1923aec223e551d4c2649195bb
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2021-11-17 16:53:02 +0100

    Finalized diversity analysis (categorical BE vs. BB version).
    Acked-by: Emanuel Heitlinger <emanuelheitlinger@gmail.com>

--- 2_iNEXT_fox.R
+++ 2_iNEXT_fox.R
@@ -1,8 +1,9 @@
 library(phyloseq)
 library(iNEXT)
 library(vegan)
+library(betapart)
 library(tidyverse)
-
+library(patchwork)
 
 recompute <- FALSE
 
@@ -21,11 +22,7 @@ if(!exists("PS")){
 ## (e.g. Coccidia) too?
 
 PSHelm <- phyloseq::subset_taxa(PS, phylum%in%c("Nematoda", "Platyhelminthes"))
-
-PSHelmS <- phyloseq::tax_glom(PSHelm, "species")
-
 PSHelmG <- phyloseq::tax_glom(PSHelm, "genus")
-
 HelmCounts <- as.data.frame(otu_table(PSHelmG))
 
 colnames(HelmCounts) <- tax_table(PSHelmG)[, "genus"]
@@ -38,132 +35,144 @@ rowSums(HelmCounts)
 ## Per Fox species numbers
 rowSums(HelmCounts>0)
 
-### First a rarefaction analysis per sample!!!
-## We need to keep only samples with more than one species
+
+### For inext diversity analysis we !
+## We need to keep only samples with at least two species
 indHelmCounts <- HelmCounts[rowSums(HelmCounts>0)>1, ]
+
+
+## Sample data in data frame
+Sdat <- as.data.frame(sample_data(PSHelmG))
+class(Sdat) <- "data.frame"
+
+## sample data for samples with helminth present
+SdatHPres <- Sdat[rowSums(HelmCounts>0)>1, ]
+
+## how many lost for each area
+table(Sdat$area, MoreOne=rowSums(HelmCounts>0)>1)
+
+## 21 samples with less than 2 species. No significant differences in
+## the areas
+fisher.test(table(Sdat$area, MoreOne=rowSums(HelmCounts>0)>1))
 
 OTU_inext_imp <- iNEXT(t(indHelmCounts), q =0, datatype = "abundance")
 
 ### Now the plot gets a bit messy redo by hand
 zet <- fortify(OTU_inext_imp)
 
-## merge it with the sample data
-Sdat <- as.data.frame(sample_data(PSHelmG))
-class(Sdat) <- "data.frame"
-
 Sdat$IZW_ID <- paste("Fox", Sdat$IZW_ID)
 
 zet <- merge(zet, Sdat, by.x="site", by.y="IZW_ID")
 
-divFoxPlot2000 <- ggplot(zet, aes(x = x, y = y, colour = area, group=site)) +
+alphaDivFox <- ggplot(zet, aes(x = x, y = y, colour = area, group=site)) +
     geom_point(data=subset(zet, method%in%"observed"), size=4) + 
     geom_line(data=subset(zet, method%in%"interpolated"),
               lwd = 1.5, alpha=0.3) +
     scale_colour_manual(values = c("#e7b800", "#2e6c61")) +
     scale_fill_manual(values = c("#e7b800", "#2e6c61")) +
-    ## guides(linetype = guide_legend(title = "Method"),
-    ##        colour = guide_legend(title = "Guides"), 
-    ##        fill = guide_legend(title = "Guides"),
-    ##        shape = guide_legend(title = "Guides")) + 
-    theme(legend.position = "bottom", legend.title = element_blank(), 
-          text = element_text(size = 18), legend.key.width = unit(1.2, 
-                                                                  "cm")) +
-    theme(legend.position = "bottom", legend.title = element_blank(), 
-          text = element_text(size = 18), legend.key.width = unit(1.2, 
-                                                                  "cm")) +
     ylab("helminth diversity\n") +
     xlab("number of sequence reads\n") +
-##    scale_x_continuous(limits=c(0, 2000)) +
     theme_bw()
 
-divFoxPlot2000
-### 52 observed values above the sequencing count shown onx!
+
+## compare the asymptotic estimates between Berlin and Brandenburg
+## But iNext loses the rownames and all extra information, add back:
+EstimatesAsy <- cbind(OTU_inext_imp$AsyEst, SdatHPres[rep(1:nrow(SdatHPres), each=3), ])
+
+
+alphaCompared <- ggplot(EstimatesAsy, aes(area, Observed, color=area)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_jitter() +
+    scale_y_continuous("") +
+    facet_wrap(~Diversity, scales="free_y")+
+    scale_colour_manual(values = c("#e7b800", "#2e6c61")) +
+    scale_fill_manual(values = c("#e7b800", "#2e6c61")) +
+    theme_minimal()
+
+EstimatesAsy %>% filter(Diversity %in% "Shannon diversity") %>%
+    lm(Observed~ area + condition + I(as.numeric(weight_kg)) + sex + age, data=.) ->
+    DivModelShannon
+
+summary(DivModelShannon)
+
+EstimatesAsy %>% filter(Diversity %in% "Simpson diversity") %>%
+    lm(Observed~ area + condition + I(as.numeric(weight_kg)) + sex + age, data=.) ->
+    DivModelSimpson
+
+summary(DivModelSimpson)
+
+
+EstimatesAsy %>% filter(Diversity %in% "Species richness") %>%
+    lm(Observed~ area + condition + I(as.numeric(weight_kg)) + sex + age, data=.) ->
+    DivModelRichness
+
+summary(DivModelRichness)
+
+
+
 
 ## Now: the way Caro designed the analysis it distinguishes between
 ## Berlin and Brandenburg as a whole (and between male and female,
 ## maybe?)
 
-## Metadata annotated presence species number per fox
+HelmPres1 <- as.data.frame(HelmCounts>0)
+HelmPres1 <- apply(HelmPres1, 1, as.numeric)
 
-HelmCountsAnn <- merge(HelmCounts, Sdat, by.x=0, by.y="IZW_ID")
+HelmPresArea <- by(t(HelmPres1), Sdat$area, function (x) x)
 
-HelmPresAbsAnn1 <- by(HelmCountsAnn, HelmCountsAnn$area,
-                     function (x) {colSums(x[, 2:ncol(HelmCounts)]>0)})
 
-HelmPresAbsAnn1 <- do.call(rbind, HelmPresAbsAnn1)
-    
-OTU_inext_area1 <- iNEXT(t(HelmPresAbsAnn1), q =0, datatype = "abundance")
+## for iNext all sites have to have more than one species!
+Fox_inext_area1 <- iNEXT(list(Berlin=t(HelmPresArea$Berlin),
+                              Brandenburg=t(HelmPresArea$Brandenburg)), 
+                         q=0, datatype = "incidence_raw")
 
-HelmPresAbsAnn100 <- by(HelmCountsAnn, HelmCountsAnn$area,
-                     function (x) {colSums(x[, 2:ncol(HelmCounts)]>100)})
-
-HelmPresAbsAnn100 <- do.call(rbind, HelmPresAbsAnn100)
-    
-OTU_inext_area100 <- iNEXT(t(HelmPresAbsAnn100), q =0, datatype = "abundance")
-
-plot_area1 <- ggiNEXT(OTU_inext_area1) +
+gammaDivFox <- ggiNEXT(Fox_inext_area1) +
     scale_colour_manual(values = c("#e7b800", "#2e6c61")) +
     scale_fill_manual(values = c("#e7b800", "#2e6c61")) +
     theme_minimal() +
     xlab("number of sampled foxes") +
     ylab("helminth diversity\n") +
-    theme(legend.position="bottom",
-          legend.box = "vertical", 
-          axis.title.x = element_text(size = 20, face = "bold"),
-          axis.title.y = element_text(size = 20, face = "bold"),
-          axis.text = element_text(size = 16),
-          legend.title = element_text(color = "#5c564b", size = 12, face = "bold"),
-          legend.text = element_text(color = "#5c564b", size = 12)) +
-    guides(size = guide_legend(override.aes = list(shape = 1)))
-
-plot_area1
+    theme(legend.position="none")
 
 
-plot_area100 <- ggiNEXT(OTU_inext_area100) +
+
+## beta diversity
+JaccPairsDist <- beta.pair(t(apply(indHelmCounts>0, 1, as.numeric)),
+                           index.family="jaccard")
+
+JaccGrups <- betadisper(JaccPairsDist[[3]], SdatHPres$area)
+
+## plot(JaccGrups, col=c("#e7b800", "#2e6c61"))
+
+### this is the proper way to do it!
+anova(JaccGrups)
+## nonparametric test shows distances in groups don't differe significantly
+## wilcox.test(JaccGrups$distances ~ JaccGrups$group)
+
+data.frame(distances=JaccGrups$distances,
+             area=JaccGrups$group) %>%
+    ggplot(aes(area, distances, color=area)) +
+    geom_boxplot(outlier.shape = NA) +
+    geom_jitter() +
+    scale_y_continuous("distance to area centroid") +
     scale_colour_manual(values = c("#e7b800", "#2e6c61")) +
     scale_fill_manual(values = c("#e7b800", "#2e6c61")) +
-    theme_minimal() +
-    xlab("number of sampled foxes") +
-    ylab("helminth diversity\n") +
-    theme(legend.position="bottom",
-          legend.box = "vertical", 
-          axis.title.x = element_text(size = 20, face = "bold"),
-          axis.title.y = element_text(size = 20, face = "bold"),
-          axis.text = element_text(size = 16),
-          legend.title = element_text(color = "#5c564b", size = 12, face = "bold"),
-          legend.text = element_text(color = "#5c564b", size = 12)) +
-    guides(size = guide_legend(override.aes = list(shape = 1)))
+    theme(legend.position="none")+
+    theme_minimal() -> betaDivJac
 
-plot_area100
 
-HelmReadAnn <- by(HelmCountsAnn, HelmCountsAnn$area,
-                  function (x) {colSums(x[, 2:ncol(HelmCounts)])})
+old_par <- par(mar = c(0, 0, 0, 0), bg = NA)
+betaDivJacMulti <- 
+    wrap_elements(full =
+                      ~(plot(JaccGrups, col=c("#e7b800", "#2e6c61"), main="",
+                             label = FALSE, sub="")))
+par(old_par)
+dev.off()
 
-HelmReadAnn <- do.call(rbind, HelmReadAnn)
-    
-Read_inext_area <- iNEXT(t(HelmReadAnn), q =0, datatype = "abundance")
 
-plot_areaSeq <- ggiNEXT(Read_inext_area) +
-    scale_colour_manual(values = c("#e7b800", "#2e6c61")) +
-    scale_fill_manual(values = c("#e7b800", "#2e6c61")) +
-    theme_minimal() +
-    xlab("number of sequence reads") +
-    ylab("helminth diversity\n") +
-    theme(legend.position="bottom",
-          legend.box = "vertical", 
-          axis.title.x = element_text(size = 20, face = "bold"),
-          axis.title.y = element_text(size = 20, face = "bold"),
-          axis.text = element_text(size = 16),
-          legend.title = element_text(color = "#5c564b", size = 12, face = "bold"),
-          legend.text = element_text(color = "#5c564b", size = 12)) +
-    guides(size = guide_legend(override.aes = list(shape = 1)))
+pdf("figures/Diversity.pdf", width=12, height=8)
+(alphaDivFox + alphaCompared) /
+    (betaDivJacMulti + betaDivJac  + gammaDivFox) +
+    plot_annotation(tag_levels = 'a')
+dev.off()    
 
-plot_areaSeq
-
-shannonFox <- vegan::diversity(HelmCounts, index="shannon")
-
-shannonPresAbs1 <- vegan::diversity(HelmPresAbsAnn1, index="shannon")
-
-shannonPresAbs100 <- vegan::diversity(HelmPresAbsAnn100, index="shannon")
-
-shannonRead <- vegan::diversity(HelmReadAnn, index="shannon")
