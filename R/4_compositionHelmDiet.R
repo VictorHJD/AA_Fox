4444fded185dafe8a7e056af2d25d2a9dde257db
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2022-01-10 16:42:45 +0100

    Worked out the compositon (nMDS) analysis

--- R/4_compositionHelmDiet.R
+++ R/4_compositionHelmDiet.R
@@ -1,5 +1,8 @@
 library(vegan)
+library(tidyverse)
+library(phyloseq)
 
+recomputeBioinfo <- FALSE
 
 if(!exists("PS")){
     if(recomputeBioinfo){
@@ -15,8 +18,14 @@ traits <- read.csv("input_data/helminth_traits.csv")
 traits %>%
     column_to_rownames("t.genus")  -> traits 
 
+
 BADtaxa <- rownames(traits)[!traits$BlastEvaluation%in%"Okay"]
 NOPara <- rownames(traits)[traits$fox.parasite%in%"No"]
+
+## a genus of tropical reef fish
+add.bad.taxa <- "Tonlesapia"
+
+BADtaxa <- c(BADtaxa, add.bad.taxa)
 
 ## collapse to genus level
 PSG <- phyloseq::tax_glom(PS, "genus")
@@ -24,7 +33,7 @@ PSG <- phyloseq::tax_glom(PS, "genus")
 PSF <- subset_taxa(PSG, !genus%in%NOPara &
                         !genus%in%BADtaxa)
 
-PSF <- subset_samples(PSG, !is.na(sample_data(PSF)[, "condition"]) &
+PSF <- subset_samples(PSF, !is.na(sample_data(PSF)[, "condition"]) &
                            !is.na(sample_data(PSF)[, "weight_kg"]))
 
 PSHelm <- subset_taxa(PSF, phylum %in% c("Nematoda", "Platyhelminthes"))
@@ -34,13 +43,14 @@ PSHelm <- prune_taxa(taxa_sums(PSHelm) > 0, PSHelm)
 PSHelm <- prune_samples(sample_sums(PSHelm) > 0, PSHelm)
 
 HelmData <- otu_table(PSHelm)
+colnames(HelmData) <- tax_table(PSHelm)[, "genus"]
 
 EnvData <- sample_data(PSHelm)
 class(EnvData) <- "data.frame"
 EnvData <- EnvData[, c("area", "weight_kg", "age", "sex", "condition")]
 EnvData$weight_kg <- as.numeric(EnvData$weight_kg)
 
-adonis2(HelmData ~ age + weight_kg + sex + condition,
+adonis2(HelmData ~ area + age + weight_kg + sex + condition,
         data=EnvData, na.action = na.omit, by="margin")
 
 
@@ -70,12 +80,76 @@ colnames(DietCollapsed) <- ifelse(tax_table(PSDietCollapsed)[, "phylum"] %in%"Ch
                                   as.vector(tax_table(PSDietCollapsed)[, "class", drop=T]))
 
 set.seed(123)
-nMDSHelm <- metaMDS(HelmData, distance = "jaccard", weakties = FALSE, try=250, trymax=250, k=4,
+nMDSHelm <- metaMDS(HelmData, distance = "jaccard", weakties = FALSE, try=1500, trymax=1500, k=3,
                     center = TRUE)
 
-HelmDietFit <- envfit(nMDSHelm, cbind(DietCollapsed, EnvData), na.rm=TRUE)
-HelmEnvFit <- envfit(nMDSHelm, EnvData, na.rm=TRUE)
-
+HelmDietEnvFit <- envfit(nMDSHelm, cbind(DietCollapsed, EnvData), na.rm=TRUE)
 ### AMAZING! ThIs MAKeS SeNSe!!!!
 
+## to see which helminths drive this
+HelmHelmFit <- envfit(nMDSHelm, HelmData, na.rm=TRUE)
 
+
+HelmDietEnvFitDf <-
+    as.data.frame(
+        rbind(cbind(scores(HelmDietEnvFit, "vectors") * ordiArrowMul(HelmDietEnvFit),
+                    pvals=HelmDietEnvFit$vectors$pvals),
+              cbind(scores(HelmDietEnvFit, "factors") * ordiArrowMul(HelmDietEnvFit),
+                    ### works only because each factor has two levels!!!
+                    pvals=rep(HelmDietEnvFit$factors$pvals, each=2)))
+    )
+
+HelmHelmDf <-
+    as.data.frame(
+        cbind(scores(HelmHelmFit, "vectors") * ordiArrowMul(HelmHelmFit),
+              pvals=HelmHelmFit$vectors$pvals)
+    )
+
+
+
+HelmDietEnvFitDf$Cat <- ifelse(rownames(HelmDietEnvFitDf)%in%colnames(DietCollapsed), "Diet",
+                               "Environmental")
+
+ScoresHelm <-  as.data.frame(scores(nMDSHelm))
+ScoresHelm <- cbind(ScoresHelm, EnvData)
+
+theme_set(theme_minimal(base_family = "Roboto", base_size = 12))
+theme_update(
+    axis.title.x = element_text(margin = margin(t = 12)),
+    axis.title.y = element_text(margin = margin(r = 12)),
+    strip.text = element_text(face = "bold", color = "black", size = 12, margin = margin(b = 10)),
+    legend.title = element_text(size = 12, face = "bold"),
+    legend.text = element_text(size = 12),
+    panel.spacing.x = unit(2, "lines"),
+    panel.grid.minor = element_blank(),
+    plot.margin = margin(rep(12, 4))
+)
+
+
+ggHelmEnv <-  ggplot(data = ScoresHelm, aes(x = NMDS1, y = NMDS2)) +
+    geom_point(data = ScoresHelm, aes(colour = area), size = 3) +
+    scale_colour_manual(values = c("#e7b800", "#2e6c61"), name = "Study area:") +
+    scale_fill_manual(values = c("#e7b800", "#2e6c61"), name = "Study area:") 
+
+ggHelmEnv +
+    geom_segment(aes(x = 0, y = 0, xend = NMDS1, yend = NMDS2, size = 0.1-pvals),
+                 data = subset(HelmDietEnvFitDf, pvals<0.1),
+                 arrow = arrow(length = unit(0.04, "npc"), angle=23)) +
+    geom_text(data = subset(HelmDietEnvFitDf, pvals<0.1), aes(x = NMDS1, y = NMDS2+0.04),
+              label = row.names(subset(HelmDietEnvFitDf, pvals<0.1)), size=4.5,
+              color=ifelse(subset(HelmDietEnvFitDf, pvals<0.1)$Cat%in%"Diet", "red", "green"))+
+              scale_size_continuous(range=c(1, 3))  -> ggHelmEnvDiet
+
+
+ggHelmEnv +
+    geom_segment(aes(x = 0, y = 0, xend = NMDS1, yend = NMDS2, size = 0.1-pvals),
+                 data = subset(HelmHelmDf, pvals<0.1),
+                 arrow = arrow(length = unit(0.04, "npc"), angle=23)) +
+    geom_text(data = subset(HelmHelmDf, pvals<0.1), aes(x = NMDS1, y = NMDS2+0.04),
+              label = row.names(subset(HelmHelmDf, pvals<0.1)), size=4.5,
+              color="blue")+
+              scale_size_continuous(range=c(1, 3)) -> ggHelmEnvHelm
+
+
+library(patchwork)
+ggHelmEnvDiet + ggHelmEnvHelm
