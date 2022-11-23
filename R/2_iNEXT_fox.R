d724fb11213a9beb1fc2521072e4dbd5c8948b42
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2022-11-23 09:59:05 +0100

    proper dep varialbe labels also for suppl. table 3

--- R/2_iNEXT_fox.R
+++ R/2_iNEXT_fox.R
@@ -424,5 +424,8 @@ gimmeModels(HelmEstimateAsy) %>%
 gimmeModels(HelmEstimateAsy) %>%
     filter(Diversity %in% c("Shannon diversity", "Simpson diversity"))%>%
     dplyr::select(model) %>% .[["model"]]%>%
-    stargazer(type = "html", out="./tables/HelmDiversityq1aq2.html")
+    stargazer(type = "html", out="./tables/HelmDiversityq1aq2.html",
+              dep.var.labels = rep(c("Shannon diversity (q=1)",
+                                     "Simpson diversity (q=2)"),
+                                   each=4))
 
