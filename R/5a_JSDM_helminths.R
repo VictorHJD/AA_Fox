25c28c6962fad28399897387ca5241c2fd3ba46a
Emanuel Heitlinger <emanuelheitlinger@gmail.com>
2023-12-12 08:57:04 +0100

    Rewrite and rerun jSDM

--- R/5a_JSDM_helminths.R
+++ R/5a_JSDM_helminths.R
@@ -10,8 +10,10 @@ library(reshape2)
 library(ggcorrplot)
 library(tidyr)
 library(forcats)
+library(GGally)
 
 source("./R/plot_setup.R")
+extrafont::loadfonts(device = "all") ## run every time
 
 ## # JSDM model for fox parasites
 
@@ -36,44 +38,34 @@ source("./R/plot_setup.R")
 
 ## Fox and helminth data from the central phyloseq object
 recomputeBioinfo <- FALSE
-if(!exists("PSG")){
-    if(recomputeBioinfo){
-        source("R/1_Fox_general_MA.R")
-    } else {
-        PSG <- readRDS(file="intermediate_data/PhyloSeqGenus.Rds")
-    }
+if(recomputeBioinfo){
+    source("R/1_Fox_general_MA.R")
+} else {
+    PSGHelm <- readRDS(file="intermediate_data/PhyloSeqGenus.Rds")
 }
 
 
-table(sample_data(PSG)[ , "season"])
 
 ## We need:
 ## 1. a matrix of species: sites in rows (foxes) and helminth genera
 ## in columns
 
-PSGHelm <- subset_taxa(PSG, category%in%"Helminth")
 
 ## Also reomve foxes with with NA for weight or na for environmental
 ## variables
-PSGHelmR <- phyloseq::prune_samples(
-                         sample_sums(PSGHelm)>0 &
-                         !is.na(as.numeric(sample_data(PSGHelm)$weight_kg)) &
-                         !is.na(sample_data(PSGHelm)$condition) &
-                         !is.na(sample_data(PSGHelm)$human_fpi_1000m) &
-                         !is.na(sample_data(PSGHelm)$tree_cover_1000m) &
-                         !is.na(sample_data(PSGHelm)$DNAng.ul) &
-                         !is.na(sample_data(PSGHelm)$DNA260.230) &
-                         !is.na(sample_data(PSGHelm)$DNA260.280), 
-                         PSGHelm)
+PSGHelmR <- phyloseq::prune_samples(sample_sums(PSGHelm)>0, 
+                                    PSGHelm)
+## the good old 131 wormy foxes
 
 HelmCounts <- as.data.frame(otu_table(PSGHelmR))
 colnames(HelmCounts) <- phyloseq::tax_table(PSGHelmR)[, "genus"]
-    
+
+## ## THIS IS IMPORTANT TO REPORT!!!
 ## keep those at least in five percent of the samples
 ntokeep <- nrow(HelmCounts)*0.05
 response_data <- HelmCounts[, colSums(HelmCounts>0)>ntokeep] %>%
     as.matrix() 
-nrow(response_data) # [1] 140
+nrow(response_data) # [1] 131
 
 ## 2. a trait data frame with the genus in rows, same name and order
 ## as in species matrix (columns)
@@ -102,7 +94,7 @@ foxes <- foxes %>%
   mutate(coords.x1 = as.numeric(coords.x1)) %>% 
     mutate(coords.x1 = ifelse(duplicated(coords.x1, coords.x2),
                              coords.x1+1, coords.x1)) 
-nrow(foxes) # [1] 139
+nrow(foxes) # [1] 131
 
 ### now we need to know how the environmental data for the foxes is
 ### correlated
@@ -111,29 +103,26 @@ nrow(foxes) # [1] 139
 ### STATISTICAL ANALYSIS, but I'll leave it here now! 
 
 foxes %>%
-    dplyr::select(weight_kg, 
-                  tree_cover_1000m, imperv_1000m, human_fpi_1000m,
-                  DNAng.ul, DNA260.230, DNA260.280) %>%
+    dplyr::select(weight_kg, sex, season,
+                  area, tree_cover_1000m, imperv_1000m, human_fpi_1000m) %>%
     mutate_all(as.numeric) %>%
     cbind(foxes$area) %>%
     ggpairs() -> Cor_Plot
 
-ggsave("figures/suppl/CorrelatPedictors.png", Cor_Plot, device="png")
+ggsave("figures/suppl/CorrelatPedictors.png", Cor_Plot,
+       width = 12.5, height = 10.5, units = "in")
 
 ### and then create a predictor dataset (without non-predictor
 ### variables) and we leave out imperv_1000m as it's highly correlated
 ### with human_fpi_1000m, which is more relevant (for diversity at least)
 
 envcov_data <- foxes %>%
-    dplyr::select(IZW_ID, area, sex, age, weight_kg, season, area, condition,
-                  human_fpi_1000m, tree_cover_1000m, DNAng.ul, DNA260.230,
-                  DNA260.280)  %>%
-    mutate_at(c("IZW_ID", "area", "sex", "age", "season",
-                "area", "condition"), as.factor) %>%
-    mutate_at(c("weight_kg", "human_fpi_1000m", "tree_cover_1000m",
-                "DNAng.ul", "DNA260.230", "DNA260.280"), as.numeric) %>% 
+    dplyr::select(IZW_ID, area, sex, weight_kg, season, area, 
+                  human_fpi_1000m, tree_cover_1000m)  %>%
+    mutate_at(c("IZW_ID", "area", "sex", "season"), as.factor) %>%
+    mutate_at(c("weight_kg", "human_fpi_1000m", "tree_cover_1000m"), as.numeric) %>% 
   filter(!is.na(season))
-nrow(envcov_data) # [1] 139
+nrow(envcov_data) # [1] 131
 
 #### now the coordinates (in the coordinate system Cedric used) for
 #### the random structure
@@ -141,13 +130,13 @@ xyData <- foxes %>%
   transmute(x.coord = coords.x1, y.coord = coords.x2) %>% 
   mutate(x.coord = as.numeric(x.coord), 
          y.coord = as.numeric(y.coord))
-nrow(xyData)  # [1] 139
+nrow(xyData)  # [1] 131
 
 ## Maybe remove the spatial random effects for computational efficiency?!
 
 ## FOR NOW also removing the foxes from the same sites here
 response_data <- response_data[rownames(envcov_data), ]
-nrow(response_data)
+nrow(response_data) # 131
 
 studyDesign <- data.frame(site = rownames(envcov_data))
 studyDesign[] <- as.data.frame(lapply(studyDesign[], factor))
@@ -163,12 +152,9 @@ verbose <- 1000
 
 
 ## Regression formula for environmental covariates
-XFormula.area = ~ sex + weight_kg + season + area +
-    condition + DNAng.ul + DNA260.230 + DNA260.280
+XFormula.area = ~ sex + weight_kg + season + area 
   
-XFormula.grad = ~ sex + weight_kg + season + human_fpi_1000m + tree_cover_1000m +
-        condition + DNAng.ul + DNA260.230 + DNA260.280
-
+XFormula.grad = ~ sex + weight_kg + season + human_fpi_1000m + tree_cover_1000m 
 
 # Regression formula for traits
 TrFormula.Genera = ~ zoonotic + lifecycle + host.range
@@ -213,10 +199,14 @@ saveRDS(PAModel_grad, "./JSDM_models/PAModel_grad_jSDM_DNA.rds")
 
 ## quick look at the models
 PAModel_area
-# Hmsc object with 140 sampling units, 11 species, 9 covariates, 5 traits and 1 random levels
+## Hmsc object with 131 sampling units, 10 species, 6 covariates, 5
+## traits and 1 random levels Posterior MCMC sampling with 3 chains
+## each with 20000 samples, thin 10 and transient 1000
 PAModel_area$XFormula
 
 PAModel_grad
-# Hmsc object with 140 sampling units, 11 species, 10 covariates, 5 traits and 1 random levels
+## Hmsc object with 131 sampling units, 10 species, 7 covariates, 5
+## traits and 1 random levels Posterior MCMC sampling with 3 chains
+## each with 20000 samples, thin 10 and transient 1000
 PAModel_grad$XFormula
 
